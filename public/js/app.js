c48c485d464486861d28b98009618ed843d92e6b
Manodeep <mmanodeep@gmail.com>
2022-06-09 09:37:55 +0530

    Init commit

---
+++ public/js/app.js
@@ -0,0 +1,32 @@
+const locationField = document.querySelector('form')
+const searchElement = document.querySelector('input')
+const msgOne = document.querySelector('#msgOne')
+const msgTwo = document.querySelector('#msgTwo')
+
+
+
+
+
+locationField.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const location = searchElement.value
+
+    msgOne.textContent = 'Loading...'
+    msgTwo.textContent = ''
+
+    fetch('http://localhost:3000/weather?address=' + location).then(response => {
+        response.json().then((data) => {
+            if (data.error) {
+                msgOne.textContent = data.error;
+                
+            }
+            else {
+                msgOne.textContent = data.location
+                msgTwo.textContent = data.forecast
+               
+            }
+
+        })
+    })
+})
